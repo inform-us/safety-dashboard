57a0c61d3cd4b415ef22e7b7feacddf36e9904a8
Katie Buntic <k.buntic@ucl.ac.uk>
2024-08-07 16:30:10 +0100

    pharmacy page

--- frontend/src/pages/Pharmacy.tsx
+++ frontend/src/pages/Pharmacy.tsx
@@ -1,8 +1,13 @@
+import Button from '../shared/components/UIElements/Button';
+import links from '../shared/links';
+
 const Pharmacy = () => {
   return (
     <div>
       <h2>Pharmacy</h2>
       <p>This is the pharmacy page.</p>
+      <Button href={links['3c']}>3c: Exemplar ward safe storage dashboard (Power BI)</Button>
+      <Button href={links['5']}>5: Clinical inpatients dashboard (Power BI)</Button>
     </div>
   );
 };
