7b5d40040f1c50e703b445605e996d4708e4e756
James Hughes <james.hughes@ucl.ac.uk>
2024-08-08 07:54:37 +0100

    formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,32 @@
 import { useState } from 'react';
-import './App.css';
+
 import AppRoutes from './AppRoutes';
 import Modal from './shared/components/UIElements/modal/Modal';
 import Button from './shared/components/UIElements/Button';
 
+import './App.css';
+
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(true); // Modal is open by default when app is loaded
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <div className="home">
-      <AppRoutes />
       <Modal
         show={isModalOpen}
-        onCancel={closeModal}
+        onCancel={() => setIsModalOpen(false)}
         header="Welcome to the UCLH Safety Dashboard"
-        footer={<Button onClick={closeModal}>Close</Button>}
+        footer={<Button onClick={() => setIsModalOpen(false)}>Close</Button>}
       >
-        <p>Please note that this dashboard is an experimental design.</p>
+        <>
+          <p>Please note that this dashboard is an experimental platform.</p>
+          <p>
+            This is a proof of concept. The technical architecture is fragile and should not be expected to run
+            smoothly.
+          </p>
+        </>
       </Modal>
+
+      <AppRoutes />
     </div>
   );
 };
